e04216e6487273156e43a746f6f79e32a1f0ec04
Prahadeeshwaran <95561624+prahadeeshwaran-k@users.noreply.github.com>
2023-07-08 15:52:01 +0530

    Create Rotary Encoder by Logic Operation

---
+++ Rotary Encoder by Logic Operation.ino
@@ -0,0 +1,38 @@
+int clkPin = 3;
+int dtPin = 4;
+int switchPin = 12;
+
+int count = 0;
+int clkPinLast = LOW;
+int clkPinCurrent = LOW;
+
+
+void setup() {
+  pinMode(clkPin, INPUT);
+  pinMode(dtPin, INPUT);
+  pinMode(switchPin, INPUT_PULLUP);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int switchState = digitalRead(switchPin);
+
+  if (switchState == LOW) {
+    count = 0;
+  }
+
+  clkPinCurrent = digitalRead(clkPin);
+
+  if ((clkPinLast == LOW) && (clkPinCurrent == HIGH)) {
+    if (digitalRead(dtPin) == HIGH) {
+      count--;
+    }
+    else {
+      count++;
+    }
+
+    Serial.println(count);
+  }
+
+  clkPinLast = clkPinCurrent;
+}
